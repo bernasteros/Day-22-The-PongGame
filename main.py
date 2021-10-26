08dfe6776de71308b98e6bd4568ca94a2dd7ff09
Bernhard Stern <bernhard.stern@keemail.me>
2021-10-26 16:19:53 +0200

    Initial commit - Todo List

---
+++ main.py
@@ -0,0 +1,10 @@
+from turtle import Screen
+
+# Todo: Establish a Basic-Screen
+# Todo: Create a Paddle and make it steerable
+# Todo: Establish a 2nd Paddle for 2nd Player
+# Todo: Create a ball and make it move
+# Todo: Establish Ball-Move and Collision Model
+# Todo: Establish Ball-Paddle Collision Model
+# Todo: Manage Paddle-Missing event (Reset?)
+# Todo: Count the goals and display them
