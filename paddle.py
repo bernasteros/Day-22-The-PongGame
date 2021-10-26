81e9213502b5cbfbd24e585d50c47466731c62a3
Bernhard Stern <bernhard.stern@keemail.me>
2021-10-26 17:35:32 +0200

    Make the Basic Paddle: - Created CONSTANTS to reconfigure later

--- paddle.py
+++ paddle.py
@@ -1,1 +1,24 @@
+from turtle import Turtle
 
+# Overall Properties
+MOVE_DISTANCE = 20
+COLOR = "white"
+SHAPE = "square"
+PADDLE_LENGTH = 4
+
+# Steering
+UP = 90
+DOWN = 270
+
+class Paddle:
+    def __init__(self):
+        self.paddle = []
+        self.segments()
+
+    def segments(self):
+        for segment in range(PADDLE_LENGTH):
+            new_square = Turtle()
+            new_square.color(COLOR)
+            new_square.shape(SHAPE)
+            new_square.penup()
+            new_square.sety(0 - 20 * segment)
