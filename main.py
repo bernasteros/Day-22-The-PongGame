92075d9ffed38b07e8620243cf1ff4e8a73a435c
Bernhard Stern <bernhard.stern@keemail.me>
2021-10-26 16:30:57 +0200

    Create a basic Screen (Turtle-Method)

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from turtle import Screen
 
-# Todo: Establish a Basic-Screen
+# Establish a Basic-Screen
+screen = Screen()
+screen.setup(width=1000, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+
 # Todo: Create a Paddle and make it steerable
+
 # Todo: Establish a 2nd Paddle for 2nd Player
+
 # Todo: Create a ball and make it move
+
 # Todo: Establish Ball-Move and Collision Model
+
 # Todo: Establish Ball-Paddle Collision Model
+
 # Todo: Manage Paddle-Missing event (Reset?)
+
 # Todo: Count the goals and display them
+
+screen.exitonclick()
